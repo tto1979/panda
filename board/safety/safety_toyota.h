fd65f0d2166ecb9bfa355a5821a3b85d61cca469
CT921 <tim.chen242@gmail.com>
2023-10-22 04:45:34 +0800

    Update safety_toyota.h

--- board/safety/safety_toyota.h
+++ board/safety/safety_toyota.h
@@ -217,13 +217,6 @@ static int toyota_tx_hook(CANPacket_t *to_send) {
         tx = 0;
       }
     }
-
-    // AleSato's automatic brakehold
-    if (addr == 0x344) {
-      if(vehicle_moving || gas_pressed || !acc_main_on) {
-        tx = 0;
-      }
-    }
   }
 
   return tx;
@@ -257,9 +250,7 @@ static int toyota_fwd_hook(int bus_num, int addr) {
     int is_lkas_msg = ((addr == 0x2E4) || (addr == 0x412) || (addr == 0x191));
     // in TSS2 the camera does ACC as well, so filter 0x343
     int is_acc_msg = (addr == 0x343);
-    // Block AEB when stoped to use as a automatic brakehold
-    int is_aeb_msg = (addr == 0x344);
-    int block_msg = is_lkas_msg || (is_acc_msg && !toyota_stock_longitudinal) || (is_aeb_msg && !vehicle_moving && acc_main_on && !gas_pressed);
+    int block_msg = is_lkas_msg || (is_acc_msg && !toyota_stock_longitudinal);
     if (!block_msg) {
       bus_fwd = 0;
     }
