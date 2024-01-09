3da046c18f7038e76ed219467768e1b80d47c0e4
CT921 <tim.chen242@gmail.com>
2024-01-09 12:50:46 +0800

    Update safety_honda.h

--- board/safety/safety_honda.h
+++ board/safety/safety_honda.h
@@ -331,12 +331,10 @@ static bool honda_tx_hook(CANPacket_t *to_send) {
 
   // STEER: safety check
   bool alka_enabled = alternative_experience & ALT_EXP_ALKA;
-  if (!alka_enabled && ((addr == 0xE4) || (addr == 0x194))) {
-    if (!alka_enabled && !controls_allowed) {
-      bool steer_applied = GET_BYTE(to_send, 0) | GET_BYTE(to_send, 1);
-      if (steer_applied) {
-        tx = false;
-      }
+  if (!alka_enabled && ((addr == 0xE4) || (addr == 0x194)) && !controls_allowed) {
+    bool steer_applied = GET_BYTE(to_send, 0) | GET_BYTE(to_send, 1);
+    if (steer_applied) {
+      tx = false;
     }
   }
 
